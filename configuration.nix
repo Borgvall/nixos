fbe35f9d999408ef99e6533e92c2b81874d24051
Johannes Röhl <joroehl@gmx.de>
2025-12-27 19:42:23 +0100

    Strip redundant comments

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
@@ -24,56 +20,36 @@
 
   hardware.graphics.enable32Bit = true;
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos-johannes"; # Define your hostname.
+  networking.hostName = "nixos-johannes";
 
-  # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "de_DE.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
 
-  # 1. Automatische Updates (inkl. Sicherheitsupdates)
   system.autoUpgrade = {
     enable = true;
     dates = "daily";
     allowReboot = false;
   };
 
-  # 2. Automatische Bereinigung (Löscht alte Generationen)
   nix.gc = {
     automatic = true;
     dates = "daily";
     options = "--delete-older-than 7d";
   };
 
-  # 3. Speicherplatz optimieren (Deduplizierung)
-  # Identische Dateien im Store werden nur einmal gespeichert
   nix.settings.auto-optimise-store = true;
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
 
 
-  # Enable the GNOME Desktop Environment.
   services.displayManager.gdm = {
     enable = true;
     wayland = true;
@@ -81,30 +57,21 @@
   services.desktopManager.gnome.enable = true;
   
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "de,de";
     variant = "neo,nodeadkeys";
     options = "grp:sclk_toggle";
   };
 
-  # Konfiguration für die Konsole (TTY)
-  # Übernimmt das Layout von oben auch für das Terminal ohne Grafikoberfläche
   console.useXkbConfig = true; 
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
   services.pipewire = {
     enable = true;
     pulse.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.johannes = {
@@ -151,8 +118,6 @@
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     ((vim-full.customize {
       vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
@@ -212,30 +177,6 @@
     ]))
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
@@ -254,6 +195,5 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
 
