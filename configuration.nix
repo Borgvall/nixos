f9b753283d4d735cef1480b03b72d33f7bc2d027
Johannes Röhl <joroehl@gmx.de>
2025-12-28 11:08:14 +0100

    Add proton-ge-bin to steam

--- configuration.nix
+++ configuration.nix
@@ -382,6 +382,9 @@
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+    ];
     #remotePlay.openFirewall = true; # Optional: Für Steam Remote Play
     #dedicatedServer.openFirewall = true; # Optional: Für Source Dedicated Server
   };
