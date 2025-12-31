adf58cff2e7d8d1080ba986fd30989ff48fa4a12
Johannes Röhl <joroehl@gmx.de>
2025-12-31 13:25:57 +0100

    Revert "Experimental: disable firewall"
    This reverts commit b8004f31f864a05e667af2807427be98acd85b73.

--- configuration.nix
+++ configuration.nix
@@ -96,7 +96,6 @@
 
   # Firewall: Port 427 (SLP) wird oft für die HP-Erkennung benötigt
   networking.firewall.allowedUDPPorts = [ 427 ];
-  networking.firewall.enable = false;
 
   services.pipewire = {
     enable = true;
