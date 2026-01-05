b210c99b2c3eb72c3f2484bbd939da286b342113
Johannes Röhl <joroehl@gmx.de>
2026-01-05 21:05:10 +0100

    Refactor btrfs-filesystems

--- btrfs-filesystems.nix
+++ btrfs-filesystems.nix
@@ -1,46 +1,43 @@
-{config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-{
-  fileSystems."/" = {
-    options = [ "subvol=nixos-root" "compress=zstd" "noatime" ];
+let
+  commonBtrfsOptions = [ "compress=zstd" "noatime" ];
+
+  createBackupFilesystem = mountpoint: uuid: {
+    fileSystems."${mountpoint}" = {
+      device = "/dev/disk/by-uuid/${uuid}";
+      fsType = "btrfs";
+      options = commonBtrfsOptions ++ [ "noauto" ];
+    };
+
+    systemd.tmpfiles.rules = [
+      "d ${mountpoint} 0755 root root -"
+    ];
   };
 
-  fileSystems."/nix" = {
-    options = [ "subvol=nixos-nix" "compress=zstd" "noatime" ];
-  };
+in
+lib.mkMerge [
+  {
+    fileSystems."/" = {
+      options = [ "subvol=nixos-root" ] ++ commonBtrfsOptions;
+    };
 
-  fileSystems."/home" = {
-    options = [ "subvol=home" "compress=zstd" "noatime" ];
-  };
+    fileSystems."/nix" = {
+      options = [ "subvol=nixos-nix" ] ++ commonBtrfsOptions;
+    };
 
-  fileSystems."/btrfs-root" = {
-    device = "/dev/disk/by-uuid/43522f60-718b-4581-a57a-561ba2e745ba";
-    fsType = "btrfs";
-    options = [ "compress=zstd" "noatime" ];
-  };
+    fileSystems."/home" = {
+      options = [ "subvol=home" ] ++ commonBtrfsOptions;
+    };
 
-  fileSystems."/backup-intern" = {
-    device = "/dev/disk/by-uuid/60d4e5b5-1d3a-4816-bb1c-fc92526ebdfd";
-    fsType = "btrfs";
-    options = [ "compress=zstd" "noatime" "noauto" ];
-  };
+    fileSystems."/btrfs-root" = {
+      device = "/dev/disk/by-uuid/43522f60-718b-4581-a57a-561ba2e745ba";
+      fsType = "btrfs";
+      options = commonBtrfsOptions;
+    };
+  }
 
-  fileSystems."/backup-btrfs" = {
-    device = "/dev/disk/by-uuid/431cccce-3c86-4a6d-9c3a-f1ba17433b73";
-    fsType = "btrfs";
-    options = [ "compress=zstd" "noatime" "noauto" ];
-  };
-
-  fileSystems."/backup-btrfs-2TB" = {
-    device = "/dev/disk/by-uuid/d24c95ce-a05a-4c55-8e19-b735c8d32fd5";
-    fsType = "btrfs";
-    options = [ "compress=zstd" "noatime" "noauto" ];
-  };
-
-  # Create mount points for backup filesystems
-  systemd.tmpfiles.rules = [
-    "d /backup-intern 0755 root root -"
-    "d /backup-btrfs 0755 root root -"
-    "d /backup-btrfs-2TB 0755 root root -"
-  ];
-}
+  (createBackupFilesystem "/backup-intern"     "60d4e5b5-1d3a-4816-bb1c-fc92526ebdfd")
+  (createBackupFilesystem "/backup-btrfs"      "431cccce-3c86-4a6d-9c3a-f1ba17433b73")
+  (createBackupFilesystem "/backup-btrfs-2TB"  "d24c95ce-a05a-4c55-8e19-b735c8d32fd5")
+]
