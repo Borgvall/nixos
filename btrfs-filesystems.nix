8b1167476a987c0ca4621bd73e7bc195e4d1d120
Johannes Röhl <joroehl@gmx.de>
2026-01-05 20:23:07 +0100

    Add more btrfs mount points

--- btrfs-filesystems.nix
+++ btrfs-filesystems.nix
@@ -12,4 +12,28 @@
   fileSystems."/home" = {
     options = [ "subvol=home" "compress=zstd" "noatime" ];
   };
+
+  fileSystems."/btrfs-root" = {
+    device = "/dev/disk/by-uuid/43522f60-718b-4581-a57a-561ba2e745ba";
+    fsType = "btrfs";
+    options = [ "compress=zstd" "noatime" ];
+  };
+
+  fileSystems."/backup-intern" = {
+    device = "/dev/disk/by-uuid/60d4e5b5-1d3a-4816-bb1c-fc92526ebdfd";
+    fsType = "btrfs";
+    options = [ "compress=zstd" "noatime" "noauto" ];
+  };
+
+  fileSystems."/backup-btrfs" = {
+    device = "/dev/disk/by-uuid/431cccce-3c86-4a6d-9c3a-f1ba17433b73";
+    fsType = "btrfs";
+    options = [ "compress=zstd" "noatime" "noauto" ];
+  };
+
+  fileSystems."/backup-btrfs-2TB" = {
+    device = "/dev/disk/by-uuid/d24c95ce-a05a-4c55-8e19-b735c8d32fd5";
+    fsType = "btrfs";
+    options = [ "compress=zstd" "noatime" "noauto" ];
+  };
 }
