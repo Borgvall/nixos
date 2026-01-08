501d6291d0b6237b4d07062f45a4a5e61b81fb74
Johannes Röhl <joroehl@gmx.de>
2026-01-08 14:29:32 +0100

    Add DICPATH environment variable

--- configuration.nix
+++ configuration.nix
@@ -165,6 +165,11 @@
     ]))
   ];
 
+  # Damit Apps die Wörterbücher auch finden:
+  environment.sessionVariables = {
+    DICPATH = "/run/current-system/sw/share/hunspell:/run/current-system/sw/share/hyphen";
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
