88e5a2d211c9aaf54ea773cdcb9b98e1504531cc
Johannes Röhl <joroehl@gmx.de>
2026-01-24 22:02:56 +0100

    Feat: start backup, when external disk plugs in

--- haskell-backup.nix
+++ haskell-backup.nix
@@ -23,10 +23,23 @@ let
       };
     };
   };
+  haskell-backup-service-starts-by-disk-availability = name: uuid:
+  let
+    escapeHyphen = fileName: lib.replaceStrings ["-"] ["\\x2d"] fileName;
+    disk-device = "dev-disk-${escapeHyphen "by-uuid"}-${escapeHyphen uuid}.device";
+  in {
+    systemd.services."haskell-${name}" = {
+      bindsTo = [ disk-device ];
+      after = [ disk-device ];
+      wantedBy = [ disk-device ];
+    };
+  };
 in
 lib.mkMerge [
   (haskell-backup-service "backup-intern")
   (haskell-backup-service-timer "backup-intern")
   (haskell-backup-service "backup")
+  (haskell-backup-service-starts-by-disk-availability "backup" "431cccce-3c86-4a6d-9c3a-f1ba17433b73")
   (haskell-backup-service "backup-2TB")
+  (haskell-backup-service-starts-by-disk-availability "backup-2TB" "d24c95ce-a05a-4c55-8e19-b735c8d32fd5")
 ]
