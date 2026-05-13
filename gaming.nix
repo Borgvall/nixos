fbb260de27c54cb4d3f2da7a028172c2966b77ea
Johannes Röhl <joroehl@gmx.de>
2026-05-13 12:19:20 +0200

    Feat(gaming): link ProtonGE to compatibilitytools.d
    This makes it available to Heroic Games Launcher and others.

--- gaming.nix
+++ gaming.nix
@@ -15,6 +15,20 @@
     #remotePlay.openFirewall = true; # Optional: Für Steam Remote Play
     #dedicatedServer.openFirewall = true; # Optional: Für Source Dedicated Server
   };
+
+  system.userActivationScripts.linkProtonGE = {
+    text = ''
+      TARGET="${pkgs.proton-ge-bin.steamcompattool.outPath}"
+      LINK="$HOME/.steam/root/compatibilitytools.d/Proton-GE"
+      
+      # Create the link only, if it's not correct already.
+      if [ ! -L "$LINK" ] || [ "$(readlink -e "$LINK")" != "$TARGET" ]; then
+        mkdir -p "$(dirname "$LINK")"
+        ln -Tfs "$TARGET" "$LINK"
+      fi
+    '';
+  };
+
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
 
