54b69a3f22a3c09bbeb2391b4850e37aef8d7ce0
Johannes Röhl <joroehl@gmx.de>
2026-05-29 22:14:42 +0200

    Chore: disable gdm

--- configuration.nix
+++ configuration.nix
@@ -54,11 +54,7 @@
 
   services.xserver.enable = true;
 
-  services.displayManager.gdm = {
-    enable = true;
-  };
   services.desktopManager.gnome.enable = true;
-  
 
   services.xserver.xkb = {
     layout = "de,de";
