790bbcf7eb55f3bfb4e6043e8d82b73e13f97113
Johannes Röhl <joroehl@gmx.de>
2026-05-29 22:30:02 +0200

    Revert "Chore: disable gdm"
    This reverts commit 54b69a3f22a3c09bbeb2391b4850e37aef8d7ce0.

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,11 @@
 
   services.xserver.enable = true;
 
+  services.displayManager.gdm = {
+    enable = true;
+  };
   services.desktopManager.gnome.enable = true;
+  
 
   services.xserver.xkb = {
     layout = "de,de";
