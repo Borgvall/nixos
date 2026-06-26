41e59d280ba935b73df226310f9f28f642519dde
Johannes Röhl <joroehl@gmx.de>
2026-06-26 23:10:32 +0200

    Feat(objectscriptenv): podman container with IRIS

--- objectscriptenv.nix
+++ objectscriptenv.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}:
 
-{
+let 
+  containerUser = "iris-development";
+  containerUserHome = "/var/home/${containerUser}";
+  containerUid = 64312;
+in {
   programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [
@@ -14,5 +18,37 @@
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
+  };
+
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.iris-development = {
+      autoStart = false;
+      podman = {
+        user = containerUser;
+      };
+      hostname = "iris-development";
+      image = "intersystems/iris-community:latest-em";
+      ports = [
+        "127.0.0.1:52773:52773"
+        "127.0.0.1:1972:1972"
+      ];
+      extraOptions = [ "--userns=keep-id" ];
+      volumes = [
+        "${containerUserHome}/iris-data:/usr/irissys/mgr"
+      ];
+    };
+  };
+
+  users.users."${containerUser}" = {
+    isSystemUser = true;
+    uid = containerUid;
+    group = containerUser;
+    createHome = true;
+    home = containerUserHome;
+    linger = true;
+  };
+  users.groups."${containerUser}" = {
+    gid = containerUid;
   };
 }
