5fd1376ed28f19d0a08834b609bb4498516d21b2
Johannes Röhl <joroehl@gmx.de>
2026-06-26 22:49:30 +0200

    Chore(flake): remove my nixpkg fork

--- flake.nix
+++ flake.nix
@@ -3,21 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    my-nixpkgs.url = "github:Borgvall/nixpkgs/ut1999-expose-isos";
   };
 
-  outputs = { nixpkgs, my-nixpkgs, ... }: let
-    pkgs-fork = import my-nixpkgs {
-      system = "x86_64-linux";
-      config.allowUnfree = true;
-    };
-    module-args = {
-      _module.args = {
-        inherit pkgs-fork;
-      };
-    };
-  in {
+  outputs = { nixpkgs, ... }:
+  {
     nixosConfigurations = {
       johannes-pc = nixpkgs.lib.nixosSystem {
         modules = [
@@ -25,8 +14,6 @@
           ./configuration.nix
           ./ai.nix
           ./gaming.nix
-
-          module-args
         ];
       };
     };
