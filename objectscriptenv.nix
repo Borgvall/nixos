09553c4d05b649be975bcb68cf2e5e5d405801a2
Johannes Röhl <joroehl@gmx.de>
2026-06-28 19:30:23 +0200

    Test(objectscriptenv): remove user from podman group

--- objectscriptenv.nix
+++ objectscriptenv.nix
@@ -66,9 +66,6 @@ in {
     createHome = true;
     home = containerUserHome;
     linger = true;
-    extraGroups = [
-      "podman"
-    ];
     
     # Ermöglicht Rootless Podman das Mapping von UIDs innerhalb des Containers
     autoSubUidGidRange = true;
