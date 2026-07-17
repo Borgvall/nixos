4d541b3918fae64a260bac2c4c4e9eb20c16105b
Johannes Röhl <joroehl@gmx.de>
2026-07-18 01:08:54 +0200

    Fix(ai): increase ollama's context length

--- ai.nix
+++ ai.nix
@@ -4,6 +4,9 @@
   services.ollama = {
     enable = true;
     package = pkgs.ollama-vulkan;
+    environmentVariables = {
+      OLLAMA_CONTEXT_LENGTH = "8192";
+    };
   };
 
   environment.systemPackages = with pkgs; [
